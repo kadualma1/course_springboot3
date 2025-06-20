27ca04db1d60dd3a953cc2a7dbe6bd6be9c81b6a
Carlos Eduardo <kadumaroso@gmail.com>
2025-06-20 16:42:48 -0300

    Service layer, component registration

--- src/main/java/com/carloseduardo/course/repositories/UserRepository.java
+++ src/main/java/com/carloseduardo/course/repositories/UserRepository.java
@@ -4,8 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.carloseduardo.course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 	
-	
-
 }
