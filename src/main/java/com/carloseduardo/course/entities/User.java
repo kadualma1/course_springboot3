a8be70089992c01b163d232140d3218a365febc4
Carlos Eduardo <kadumaroso@gmail.com>
2025-06-20 18:50:35 -0300

    Order entity, instant and ISO 8601 standard - 1

--- src/main/java/com/carloseduardo/course/entities/User.java
+++ src/main/java/com/carloseduardo/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.carloseduardo.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +21,9 @@ public class User implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String name, email, phone, password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {}
 
@@ -69,6 +75,12 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -86,7 +98,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 	
 	
 }
