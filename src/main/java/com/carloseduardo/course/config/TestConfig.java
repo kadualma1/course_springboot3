b27ae63990b085ef6aa167141ed5df6d71dddae5
Carlos Eduardo <kadumaroso@gmail.com>
2025-06-21 19:24:10 -0300

    Payment Entity

--- src/main/java/com/carloseduardo/course/config/TestConfig.java
+++ src/main/java/com/carloseduardo/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.carloseduardo.course.entities.Category;
 import com.carloseduardo.course.entities.Order;
 import com.carloseduardo.course.entities.OrderItem;
+import com.carloseduardo.course.entities.Payment;
 import com.carloseduardo.course.entities.Product;
 import com.carloseduardo.course.entities.User;
 import com.carloseduardo.course.entities.enums.OrderStatus;
@@ -45,7 +46,7 @@ public class TestConfig implements CommandLineRunner {
 	
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.CANCELED, u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 			
 }
